1fa38b1d631caa573434f22f5c050373566e96ab
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-01 03:13:03 +0200

    added Validator class and validation methods to it

--- Classes/Author.rb
+++ Classes/Author.rb
@@ -1,3 +1,5 @@
+require './Helpers/Validator.rb'
+
 class Author
   attr_accessor :name, :biography
 
@@ -5,9 +7,15 @@ class Author
     @name, @biography = name, biography
   end
 
-  def self.store(name, biography)
-    self.new(name, biography)
+  def self.store(name, biography = nil)
+    if Validator.validate(name, ['string', 'not empty'])
+      puts 'Author stored successfully!'
+      self.new(name, biography)
+    else
+      Validator.error
+    end
   end
+
 end
 
 author = Author.store('Andrei', '24-12-1998')

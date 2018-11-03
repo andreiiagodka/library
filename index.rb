f9cc2bf5a1a3ac049752f8afdcb56abacc04edf1
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-03 23:22:34 +0200

    required files to index.rb

--- index.rb
+++ index.rb
@@ -1,1 +1,10 @@
-puts 'Library!'
+require './classes/author.rb'
+require './classes/book.rb'
+require './modules/validator.rb'
+require './helpers/errors.rb'
+
+author = Author.new('author', '24-12-1998')
+puts author
+
+book = Book.new('Book', 'Andrei')
+puts book

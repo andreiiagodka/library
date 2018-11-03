7155f37be955010ef5cd90bb16f0b37b19dbd94e
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-03 23:48:02 +0200

    customized exceptions

--- helpers/errors.rb
+++ helpers/errors.rb
@@ -1,3 +1,17 @@
-NOT_STRING = 'Input is not a string!'
-EMPTY_STRING = 'Input is empty!'
-NOT_INSTANCE = 'Input is not an instance of necessary class!'
+class NotStringError < StandardError
+  def initialize(msg='Input is not a string!')
+    super
+  end
+end
+
+class EmptyStringError < StandardError
+  def initialize(msg='Input is empty!')
+    super
+  end
+end
+
+class NotInstanceError < StandardError
+  def initialize(msg='Input is not an instance of necessary class!')
+    super
+  end
+end

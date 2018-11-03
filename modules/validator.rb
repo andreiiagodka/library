6db46c91d6dbfd78d9abdab575c8893d558560ab
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-03 23:48:47 +0200

    added exceptions to validator.rb

--- modules/validator.rb
+++ modules/validator.rb
@@ -1,29 +1,25 @@
 module Validator
-  def validate_string(string)
-    is_string(string) && is_not_empty(string)
-  end
-
-  def validate_instance(var, class_name)
-    is_instance_of(var, class_name)
-  end
-
-  protected
-
   def is_string(var)
-    is_string = var.is_a? String
-    puts NOT_STRING unless is_string
-    is_string
+    begin
+      raise NotStringError unless var.is_a? String
+    rescue NotStringError => e
+      puts e.message
+    end
   end
 
   def is_not_empty(var)
-    is_not_empty = !var.empty?
-    puts EMPTY_STRING unless is_not_empty
-    is_not_empty
+    begin
+      raise EmptyStringError unless !var.empty?
+    rescue EmptyStringError => e
+      puts e.message
+    end
   end
 
   def is_instance_of(var, class_name)
-    is_instance_of = var.instance_of? class_name
-    puts NOT_INSTANCE unless is_instance_of
-    is_instance_of
+    begin
+      raise NotInstanceError unless var.instance_of? class_name
+    rescue NotInstanceError => e
+      puts e.message
+    end
   end
 end

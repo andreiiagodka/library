751d1c43133a181c0adfb68dcd50e0d186f5bc7e
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-04 13:22:41 +0200

    added Order class

--- index.rb
+++ index.rb
@@ -1,9 +1,12 @@
+require 'date'
 require './modules/validator.rb'
 require './helpers/errors.rb'
 require './entities/author.rb'
 require './entities/book.rb'
 require './entities/reader.rb'
+require './entities/order.rb'
 
 author = Author.new('Andrei', '24-12-1998')
 book = Book.new('New Book', author)
-reader = Reader.new('Andrei', 'andrei.iagodka@gmail.com', 'Dnepr', 'Ruby Garage', 20)
+reader = Reader.new('Andrei', 'andrei.iagodka@gmail.com', 'Dnepr', 'Street', 20)
+order = Order.new(book, reader)

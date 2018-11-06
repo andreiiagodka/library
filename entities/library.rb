191867ab7259b20232b5579d3892de03be02a2c9
andreiiagodka <andrei.iagodka@gmail.com>
2018-11-07 01:03:50 +0200

    refactored code and methods' names at Library class

--- entities/library.rb
+++ entities/library.rb
@@ -12,93 +12,74 @@ class Library
     @orders  = []
   end
 
-  def add_author(author)
-    @authors << author if is_instance_of(author, Author)
-  end
-
-  def add_book(book)
-    @books << book if is_instance_of(book, Book)
-  end
-
-  def add_reader(reader)
-    @readers << reader if is_instance_of(reader, Reader)
-  end
-
-  def add_order(order)
-    @orders << order if is_instance_of(order, Order)
+  def add(entity)
+    case entity
+    when Author then @authors << entity
+    when Book then @books << entity
+    when Reader then @readers << entity
+    when Order then @orders << entity
+    end
   end
 
   def top_readers(quantity = 1)
     sort_hash(@readers, quantity)
   end
 
-  def top_books(quantity = 1, output = nil)
+  def top_books(quantity = 1, output = true)
     sort_hash(@books, quantity, output)
   end
 
-  def readers_number(quantity = 3)
+  def unique_readers_number(quantity = 3)
     books = top_books_array(quantity)
     output_unique_readers_number(books)
   end
 
   protected
 
-  def sort_hash(entities, quantity, output = nil)
+  def sort_hash(entities, quantity, output = true)
     return unless validate_entity(entities, quantity)
 
-    hash = create_hash(entities)
-    sorted_hash(hash, quantity, output)
+    hash = create_statistic_hash(entities)
+    sort_statistic_hash(hash, quantity, output)
   end
 
   def validate_entity(entity, quantity)
     are_entities_set(entity) && over_quantity(entity, quantity)
   end
 
-  def create_hash(entities)
+  def create_statistic_hash(entities)
     hash = {}
     entities.each do |entity|
-      case entity
-      when Reader then entity_key = entity.name
-      when Book then entity_key = entity.title
-      end
-
+      entity_key = case entity
+                   when Reader then entity.name
+                   when Book then entity.title
+                   end
       hash.store(entity_key, 0)
     end
-
-    count_orders(hash, entities[0])
+    result_orders_statistic(hash, entities[0])
     hash
   end
 
-  def count_orders(hash, entity)
+  def result_orders_statistic(hash, entity)
     @orders.each do |order|
-      case entity
-      when Reader then id = order.reader.name
-      when Book then id = order.book.title
-      end
-
+      id = case entity
+           when Reader then order.reader.name
+           when Book then order.book.title
+           end
       hash.each { |key, value| hash[key] = value + 1 if key == id }
     end
   end
 
-  def sorted_hash(hash, quantity, output)
+  def sort_statistic_hash(hash, quantity, output)
     sorted_hash = hash.sort_by(&:last).reverse.to_a
-
-    output ? sorted_hash : output_sorted_hash(sorted_hash, quantity)
+    output ? output_sorted_hash(sorted_hash, quantity) : sorted_hash
   end
 
   def output_sorted_hash(sorted_hash, quantity)
     endpoint = quantity - 1
-
     (0..endpoint).each do |key, _value|
       puts "#{sorted_hash[key][0]}: #{sorted_hash[key][1]}"
     end
-  end
-
-  def top_books_array(quantity)
-    books = []
-    top_books(quantity, true).each { |book| books << book[0] }
-
-    books
   end
 
   def output_unique_readers_number(books)
@@ -106,7 +87,12 @@ class Library
     @orders.each do |order|
       readers << order.reader.name if books.include? order.book.title
     end
+    puts "Number of Readers of the Most Popular Books - #{readers.length}"
+  end
 
-    puts readers.length
+  def top_books_array(quantity)
+    books = []
+    top_books(quantity, false).each { |book| books << book[0] }
+    books
   end
 end
